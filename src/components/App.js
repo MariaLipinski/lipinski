2e9050135cbc4e8a86488b9861cec100ace6a010
MariaLipinski <dudalipinski@hotmail.com>
2020-03-07 22:07:50 -0300

    Remoção de arquivos inutilizaveis e inicio de estruturação

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      teste
+      </header>
+    </div>
+  );
+}
+
+export default App;
